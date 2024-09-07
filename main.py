42471146406e85c7bc43301a2952c2fcd76e7c9d
Andrew2797 <andrijgolub27@gmail.com>
2024-09-07 08:41:55 +0300

    problem fix

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
- 
+from flask import Flask , render_template
+
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/page_2/")
+def page_2():
+    return render_template("page_2.html")
