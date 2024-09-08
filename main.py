da9f26e59ff36cff2795879ef3d3ef7718a6174e
Andrew2797 <andrijgolub27@gmail.com>
2024-09-08 11:19:15 +0300

    fix

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from flask import Flask , render_template
 app=Flask(__name__)
 
 
-@app.route("/")
+@app.get("/")
 def index():
     return render_template("index.html")
 
-@app.route("/page_2/")
+@app.get("/page_2/")
 def page_2():
     return render_template("page_2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
